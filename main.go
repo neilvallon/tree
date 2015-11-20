628cfbacb4b16fe71268d700579fc1eaea2e2416
Neil Vallon <neilvallon@gmail.com>
2015-11-20 07:30:37 -0600

    build full directory tree before printing

--- main.go
+++ main.go
@@ -9,30 +9,60 @@ import (
 
 func main() {
 	path := "./"
-	files, err := ioutil.ReadDir(path)
+
+	tree, err := NewTree(path)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	printFiles(path, "", files)
+	tree.Print("")
 }
 
-func printFiles(path, prefix string, files []os.FileInfo) {
-	for _, f := range files {
-		// path
-		if f.IsDir() {
-			p := path + "/" + f.Name()
-			files, err := ioutil.ReadDir(p)
+type tree struct {
+	name  string
+	files []os.FileInfo
+	dirs  []tree
+}
 
+func NewTree(root string) (tree, error) {
+	item, err := os.Stat(root)
+	if err != nil {
+		return tree{}, err
+	}
+
+	return newTree("", item)
+}
+
+func newTree(path string, dir os.FileInfo) (tree, error) {
+	prefix := path + dir.Name() + "/"
+
+	list, err := ioutil.ReadDir(prefix)
+	if err != nil {
+		return tree{}, err
+	}
+
+	parent := tree{name: dir.Name()}
+	for _, child := range list {
+		if child.IsDir() {
+			t, err := newTree(prefix, child)
 			if err != nil {
-				log.Fatalln(err)
+				return tree{}, err
 			}
-
-			printFiles(p, prefix+"-", files)
-			continue
+			parent.dirs = append(parent.dirs, t)
+		} else {
+			parent.files = append(parent.files, child)
 		}
+	}
 
-		// file
-		fmt.Println(prefix, f.Name())
+	return parent, nil
+}
+
+func (t tree) Print(prefix string) {
+	for _, child := range t.dirs {
+		fmt.Println(prefix+"|", child.name)
+		child.Print(prefix + "  ")
+	}
+	for _, child := range t.files {
+		fmt.Println(prefix+"-", child.Name())
 	}
 }
